a685471ca2f91dbf9b52e0f1fa82bae53917c275
Camilo Bernal Galindo <camilobern@gmail.com>
2015-09-01 11:10:43 -0400

    Finished, need styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 //TODO: Add NULL checks, proper dates
 //TODO: update scoreboard every time we click on date picker or next/previous date links
+
+// Constants
+var BLUE_JAYS = "Blue Jays";
+var teams = {
+    HOME: 0,
+    AWAY: 1,
+    NONE: -1
+};
 setup_date_widget();
 
 update_scoreboard();
@@ -77,15 +85,23 @@ function clear_scoreboard(){
     }
 }
 
+// get match winner
+function get_winner(match){
+    var home_wins = parseInt(match.home_win);
+    var away_wins = parseInt(match.away_win);
+
+    if (home_wins > away_wins) {
+        return teams.HOME;
+    } else if ( home_wins < away_wins){
+        return teams.AWAY;
+    }
+    return teams.NONE;
+}
+
+// Generate score board template and populate it with match information
 function make_scores_board(games) {
     // create score board list
     var scores_list = document.getElementById('scores-list');
-
-    // append date item to score board
-    //var date_item = document.getElementById('date');
-    //var date_text = games.day + "/" + games.month + "/" + games.year;
-    //date_item.appendChild(document.createTextNode(date_text));
-    //scores_list.appendChild(date_item);
 
     var matches = games.game;
     for(var i = 0; i < matches.length; i++){
@@ -102,6 +118,7 @@ function make_scores_board(games) {
         var home_team_wins = document.createElement('p');
         home_team_name.className = 'team-name';
         home_team_wins.className = 'team-wins';
+        home_team_item.className = 'home-team'
 
         // away team information template
         var away_team_item = document.createElement('li');
@@ -109,6 +126,7 @@ function make_scores_board(games) {
         var away_team_wins = document.createElement('p');
         away_team_name.className = 'team-name';
         away_team_wins.className = 'team-wins';
+        away_team_item.className = 'away-team'
 
         // match status template
         var match_status_item = document.createElement('li');
@@ -129,6 +147,14 @@ function make_scores_board(games) {
         away_team_item.appendChild(away_team_wins);
         match_info.appendChild(away_team_item);
 
+        // highlight winner
+        var winner = get_winner(matches[i]);
+        if(winner == teams.HOME){
+            home_team_item.className = 'team-winner'
+        } else if (winner == teams.AWAY){
+            away_team_item.className = 'team-winner'
+        }
+
         //populate status info
         var match_status = (matches[i].status).status;
         match_status_item.appendChild(document.createTextNode(match_status));
@@ -136,6 +162,13 @@ function make_scores_board(games) {
 
         // append sublist match item
         match_item.appendChild(match_info);
+
+        // prepend to scoreboard if this is a Blue Jays match
+        if(home_team_name.innerHTML == BLUE_JAYS ||
+            away_team_name.innerHTML == BLUE_JAYS){
+            scores_list.insertBefore(match_item, scores_list.childNodes[0]);
+            continue;
+        }
 
         // append match item to scores list
         scores_list.appendChild(match_item);
